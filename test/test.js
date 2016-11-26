12d101ca08b77ad8d4b930c5a39c2ddc8fd17301
Ophir LOJKINE <pere.jobs@gmail.com>
2016-11-26 20:41:18 +0300

    Add tests

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var linear = require("../gauss-jordan.js");
+var assert = require('assert');
+
+describe('linear', function() {
+  describe('#solve()', function() {
+    it('should solve linear systems', function() {
+      assert.deepEqual([1,2], linear.solve([[1,2],[3,4]], [5,11]));
+    });
+  });
+
+  describe('#invert()', function() {
+    it('sould invert matrices', function() {
+      assert.deepEqual([[1, -2],
+                        [0, 1]],
+                      linear.invert([[1, 2],
+                                     [0, 1]]))
+    });
+  });
+);
+
